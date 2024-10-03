b76531a8b5f9989941c3b165810926d05912b952
Olga1998-power <olgavelikaa915@gmail.com>
2024-10-03 12:00:03 +0300

    Update get_translate.cs

--- C#/get_translate.cs
+++ C#/get_translate.cs
@@ -5,7 +5,9 @@ var options = new RestClientOptions("https://api-b2b.backenster.com/b1/api/v3/tr
 var client = new RestClient(options);
 var request = new RestRequest("");
 request.AddHeader("accept", "application/json");
+request.AddHeader("Authorization", "Your authorization API key");
 request.AddJsonBody("{\"from\":\"en_GB\",\"to\":\"de_DE\",\"data\":\"London is the capital and largest city of England and of the United Kingdom.\",\"platform\":\"api\"}", false);
 var response = await client.PostAsync(request);
 
 Console.WriteLine("{0}", response.Content);
+
